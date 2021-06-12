b4ac6b71c9b61c7022b0b93c5c6ee943749b86bd
Connor Wilson <cnwilson29@gmail.com>
2021-06-12 00:39:54 -0400

    stt

--- lib/talk-page.dart
+++ lib/talk-page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class TalkPage extends StatefulWidget {
   const TalkPage({ Key key }) : super(key: key);
@@ -8,6 +10,36 @@ class TalkPage extends StatefulWidget {
 }
 
 class _TalkPageState extends State<TalkPage> {
+
+  @override
+  void initState() {
+    setupSST();
+    super.initState();
+  }
+
+  void setupSST() async {
+    stt.SpeechToText speech = stt.SpeechToText();
+    bool available = await speech.initialize( onStatus: statusListener, onError: errorListener );
+    if ( available ) {
+        speech.listen( onResult: resultListener );
+    }
+    else {
+        print("The user has denied the use of speech recognition.");
+    }
+  }
+
+  statusListener(status) {
+
+  }
+
+  errorListener(error) {
+
+  }
+
+  resultListener(SpeechRecognitionResult result) {
+    print(result.finalResult ?? '');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
